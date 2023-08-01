395544c141feb43617837663df36edf73252ed3f
GeoTsinis <geo.tsinis@gmail.com>
2023-08-01 16:08:47 +0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,115 @@
+import express from 'exress';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import multer from 'multer';
+import fs from 'fs';
+
+import User from './models/User';
+import Post from './models/Post';
+
+const app = express();
+
+const uploadingMiddleware = multer({ dest: 'uploads/' });
+
+const salt = bcrypt.genSaltSync(10);
+const secret = '';
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+mongoose.connect('');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt);
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        // Logged In
+        jwt.sign({username,id: userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie( 'token', token).json({
+                id: userDoc._id,
+                username,
+            });
+        });
+    } else {
+        res.status(400).json('wrong username or password');
+    }
+});
+
+app.get('/profile', (req,res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+});
+
+app.post('/post', uploadingMiddleware.single('file'), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length -1 ];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { id, title, summary, content } = req.body;
+        const postDoc = await Post.findById(id);
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+
+        if (!isAuthor) {
+            return res.status(400).json('you are not authorized to do that');
+        }
+
+        await postDoc.update({
+            title,
+            summary,
+            content,
+            cover: newPath ? newPath : postDoc.cover,
+        });
+
+        res.json(postDoc)
+    });
+
+});
+
+app.get('/post', async (req, res) => {
+    res.json(
+        await Post.find()
+            .populate( 'author', ['username'])
+            .sort({createdAt: -1})
+            .limit(20)
+    );
+});
+
+app.get('/post/:id', async (req, res) => {
+    const { id } = req.params;
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc);
+});
+
+app.listen(3000);
